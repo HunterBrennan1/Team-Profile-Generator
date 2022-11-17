a10384f5e502fbdaadc29c46a39dc0d13406db47
Hunter <hunterbrennan15@gmail.com>
2022-11-17 14:51:52 -0500

    created profile tests for employee and engineer

---
+++ tests/Engineer.test.js
@@ -0,0 +1,60 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Engineer", () => {
+  const testEngineer = {
+    name: "Nick",
+    id: 102,
+    email: "Nick@gmail.com",
+    github: "Nick1",
+    role: "Engineer",
+  };
+
+  const obj = new Engineer(
+    testEngineer.name,
+    testEngineer.id,
+    testEngineer.email,
+    testEngineer.github
+  );
+
+  describe("Constructor", () => {
+    it("should create a new construct employee class", () => {
+      expect(obj).toEqual({
+        name: "Nick",
+        id: 102,
+        email: "Nick@gmail.com",
+        github: "Nick1",
+        role: "Engineer",
+      });
+    });
+  });
+
+  describe("getName", () => {
+    it("should return name when getName method is called", () => {
+      expect(obj.getName()).toEqual("Nick");
+    });
+  });
+
+  describe("Id", () => {
+    it("should return id when getId method is called", () => {
+      expect(obj.getId()).toEqual(102);
+    });
+  });
+
+  describe("Email", () => {
+    it("should return email when getEmail method is called", () => {
+      expect(obj.getEmail()).toEqual("Nick@gmail.com");
+    });
+  });
+
+  describe("GitHub", () => {
+    it("should return email when getGithub method is called", () => {
+      expect(obj.getGithub()).toEqual("Nick1");
+    });
+  });
+
+  describe("Role", () => {
+    it("should return email when getRole method is called", () => {
+      expect(obj.getRole()).toEqual("Engineer");
+    });
+  });
+});
